ab9addd69750c168aeb142e724e1f12d707608e4
Rodrigo Erthal <rrerthal@gmail.com>
2023-09-14 17:18:44 +0200

    react character details info

---
+++ axios react/react-characters-app/src/App.jsx
@@ -0,0 +1,62 @@
+import { useEffect, useState } from 'react'
+import axios from "axios";
+import { Link, NavLink, Route, Routes } from 'react-router-dom';
+
+import './App.css'
+import CharacterDetails from './components/CharacterDetails';
+
+
+function App() {
+
+  const [characters, setCharacters] = useState(null);
+
+  useEffect(() => {
+    axios.get("https://ih-crud-api.herokuapp.com/characters")
+      .then(response => {
+        setCharacters(response.data);
+      })
+      .catch((e) => {
+        console.log("error getting characters from the API....", e);
+      });
+  }, []);
+
+
+
+  const renderList = () => {
+    if (characters === null) {
+      return <p>loading....</p>
+    }
+
+    return characters.map((characterObj, index) => {
+      return (
+        <section key={index} className='card' >
+          <h3>{characterObj.name}</h3>
+          <p>{characterObj.occupation}</p>
+          <Link to={"/characters/" + characterObj.id}>More Details</Link>
+        </section>
+      )
+    })
+  }
+
+
+  return (
+    <>
+
+      <nav>
+        <NavLink to="/">Home</NavLink> |
+        <NavLink to="/contact">Contact</NavLink> |
+        <NavLink to="/about">About</NavLink> |
+      </nav>
+
+      <Routes>
+        <Route path='/' element={renderList()} />
+        <Route path='/contact' element={<p>this will be the CONTACT PAGE</p>} />
+        <Route path='/about' element={<p>this will be the ABOUT PAGE</p>} />    
+        <Route path='/characters/:characterId' element={<CharacterDetails />} />
+      </Routes>
+      
+    </>
+  )
+}
+
+export default App
